6bbf4ee1cd7d192d2a05b0ee8a0777e2f962b452
David Bernal <leo.bernal1946@gmail.com>
2020-08-02 13:33:31 -0500

    first commit

---
+++ spring-cloud-consumer/src/main/java/com/example/rabbi/springcloudconsumer/SpringCloudConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabbi.springcloudconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConsumerApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-producer/src/main/java/com/example/rabbit/springcloudproducer/SpringCloudProducerApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabbit.springcloudproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudProducerApplication.class, args);
+    }
+
+}
