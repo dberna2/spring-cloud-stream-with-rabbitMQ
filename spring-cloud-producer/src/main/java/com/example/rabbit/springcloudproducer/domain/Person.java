bfb2043aac6926e5b8690e0d221968d39319180e
David Bernal <leo.bernal1946@gmail.com>
2020-08-06 18:03:34 -0500

    Add person class

---
+++ spring-cloud-producer/src/main/java/com/example/rabbit/springcloudproducer/domain/Person.java
@@ -0,0 +1,13 @@
+package com.example.rabbit.springcloudproducer.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Person {
+
+    private String id;
+    private String name;
+    private int age;
+}
